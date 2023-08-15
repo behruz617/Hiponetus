ff95a236058eadbb574b7f0a3548915e2b676c44
behruz617 <78649853+behruz617@users.noreply.github.com>
2023-08-15 22:29:46 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("1.Ededi daxil edin ");
+        System.out.println("1.Enter the number ");
         int a=sc.nextInt();
 
-        System.out.println("2.Ededi daxil edin ");
+        System.out.println("2.Enter the number ");
         int b=sc.nextInt();
 
         double c=Math.sqrt((a*a)+(b*b));
 
-        System.out.println("Hipotenuz : "+(int)c);
+        System.out.println("hypotenuse : "+(int)c);
     }
 }
